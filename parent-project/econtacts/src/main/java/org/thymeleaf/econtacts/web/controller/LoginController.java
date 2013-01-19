f198e79d10fe9781f147b58daff3d295daf464d0
unknown <velmuruganv@SISBLRLPT045.sigmainfo.com>
2013-01-19 23:27:46 +0530

    intial project layout using bootstrap and thymeleaf.

--- parent-project/econtacts/src/main/java/org/thymeleaf/econtacts/web/controller/LoginController.java
+++ parent-project/econtacts/src/main/java/org/thymeleaf/econtacts/web/controller/LoginController.java
@@ -1,5 +1,19 @@
 package org.thymeleaf.econtacts.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
+
+	@RequestMapping({ "/", "/login" })
+	public String showLoginPage() {
+		return "login";
+	}
+
+	@RequestMapping({ "/home.html" })
+	public String showUserHomePage() {
+		return "home";
+	}
 
 }
